1f5d6661131ba7d392144dfd0f94c21f63df485d
仲優樹 <yu-ki216m@outlook.jp>
2025-06-26 12:19:44 +0900

    Initial Commit

---
+++ QuizCombine/QuizCombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizCombineApp.swift
+//  QuizCombine
+//
+//  Created by 仲優樹 on 2025/06/26.
+//
+
+import SwiftUI
+
+@main
+struct QuizCombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
